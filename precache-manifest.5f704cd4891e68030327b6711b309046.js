1c3f60edac71323670ea0b6ba7373f5c96fd0377
prathvirajkanchan <46959248+prathvirajkanchan@users.noreply.github.com>
2019-02-01 10:27:51 +0530

    Add files via upload

---
+++ precache-manifest.5f704cd4891e68030327b6711b309046.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "28615ab43854e62a2cfe",
+    "url": "/static/js/main.28615ab4.chunk.js"
+  },
+  {
+    "revision": "1fa107455285891a65cd",
+    "url": "/static/js/1.1fa10745.chunk.js"
+  },
+  {
+    "revision": "28615ab43854e62a2cfe",
+    "url": "/static/css/main.f43aaa81.chunk.css"
+  },
+  {
+    "revision": "1fa107455285891a65cd",
+    "url": "/static/css/1.c46e8cfa.chunk.css"
+  },
+  {
+    "revision": "a044f175fd2332b1936c1b5ed49fec1b",
+    "url": "/index.html"
+  }
+];
